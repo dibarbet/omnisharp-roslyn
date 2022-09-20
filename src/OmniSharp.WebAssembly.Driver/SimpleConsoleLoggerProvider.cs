8dfcd766a49e12e467b33d91f374d3eebdf19a72
David Barbet <dabarbet@microsoft.com>
2022-09-20 16:52:31 -0700

    Create a partially working mef composition in omnisharp wasm

---
+++ src/OmniSharp.WebAssembly.Driver/SimpleConsoleLoggerProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+namespace OmniSharp.WebAssembly.Driver
+{
+    /// <summary>
+    /// The default console logger throws a platform not supported exception in wasm.
+    /// This implements a super duper straightforward version that doesn't do anything tricky and works in wasm.
+    /// </summary>
+    class SimpleWasmConsoleLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new SimpleWasmConsoleLogger(categoryName);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        private class SimpleWasmConsoleLogger : ILogger
+        {
+            private readonly string _categoryName;
+            public SimpleWasmConsoleLogger(string categoryName)
+            {
+                _categoryName = categoryName;
+            }
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return NullScope.Instance;
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                if (eventId == default)
+                {
+                    Console.WriteLine($"[{logLevel}][{_categoryName}]{formatter(state, exception)}");
+                }
+                else
+                {
+                    Console.WriteLine($"[{logLevel}][{_categoryName}][{eventId}]{formatter(state, exception)}");
+                }
+            }
+
+            private class NullScope : IDisposable
+            {
+                public static NullScope Instance => new();
+                public void Dispose()
+                {
+                }
+            }
+        }
+    }
+}
